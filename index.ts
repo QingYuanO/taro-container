0e94d33cc39d9b8b2440dabe9ae35d91d5711a2c
chenhu <chenhu@reedsec.com>
2021-11-08 15:16:44 +0800

    a

---
+++ index.ts
@@ -0,0 +1,10 @@
+import Container from "./src/components/Container/index";
+import {
+  useIsIPhoneX,
+  useNavBarHeight,
+  useSafeArea,
+  useScreenLayout,
+} from "./src/components/Container/hooks";
+
+export { useIsIPhoneX, useNavBarHeight, useSafeArea, useScreenLayout };
+export default Container;
